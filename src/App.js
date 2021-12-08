4853b4e2998cabf13560de8e5af74b9ba9114dcf
Divs24 <divyanshjpr01@gmail.com>
2021-12-08 23:49:19 +0530

    adding course.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import "./css/App.css";
 import Header from "./screens/header";
 import Sidebar from "./screens/sidebar";
 import HomePage from "./screens/home";
+import CoursePage from "./screens/course";
 import Rightbar from "./screens/rightbar";
 
 function App() {
   return (
     <div className="App flex">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="app-content">
-        <HomePage />
+        <CoursePage />
       </div>
-      <Rightbar />
+      {/* <Rightbar /> */}
     </div>
   );
 }
